f1c06e9205d3f45fb2e207d7ceb62f43b3d26d1d
mlulucky <dmswjdans@gmail.com>
2024-01-14 18:54:03 +0900

    A/B

---
+++ src/main/java/boj/sprout/BOJ_1008.java
@@ -0,0 +1,13 @@
+package boj.sprout;
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print((double) a / b);
+    }
+}
