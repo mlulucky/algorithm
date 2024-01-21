010ce7aa047f92e58dae8b0d34ae70bb2a21b33f
mlulucky <dmswjdans@gmail.com>
2024-01-22 00:24:34 +0900

    N 찍기

---
+++ src/main/java/boj/sprout/BOJ_2741.java
@@ -0,0 +1,13 @@
+package boj.sprout;
+
+import java.util.Scanner;
+
+public class BOJ_2741 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for(int i=1; i<=N; i++) {
+      System.out.println(i);
+    }
+  }
+}
