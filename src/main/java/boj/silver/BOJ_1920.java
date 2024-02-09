048443e80d2e1790577136475312adf1fefbc887
mlulucky <dmswjdans@gmail.com>
2024-02-10 00:03:41 +0900

    fix: 수 찾기

--- src/main/java/boj/silver/BOJ_1920.java
+++ src/main/java/boj/silver/BOJ_1920.java
@@ -11,20 +11,34 @@ public class BOJ_1920 {
     int N = Integer.parseInt(br.readLine());
     Set<Integer> set = new HashSet<>();
     StringTokenizer st = new StringTokenizer(br.readLine());
-    while(N-- > 0) {
+    while (N-- > 0) {
       set.add(Integer.parseInt(st.nextToken()));
     }
     int M = Integer.parseInt(br.readLine());
     st = new StringTokenizer(br.readLine());
 
     StringBuilder sb = new StringBuilder();
-    while(M-- > 0) {
+    while (M-- > 0) {
       int mNum = Integer.parseInt(st.nextToken());
       sb.append(set.contains(mNum) ? "1\n" : "0\n");
     }
     System.out.println(sb);
   }
 }
+
+// 참고할만한 번외 문제풀이(숏코딩 _ 다만 시간은 좀더 길어짐)
+//Scanner sc = new Scanner(System.in);
+//Set<Integer> set = new HashSet<>();
+//int n = sc.nextInt();
+//while(n-- > 0) {
+//  set.add(sc.nextInt());
+//        }
+//
+//int m = sc.nextInt();
+//while(m -->0) {
+//  System.out.println(set.contains(sc.nextInt()) ? 1 : 0);
+//        }
+
 
 // N 개 정수 -> X 라는 정수가 존재하는지 알아내기
 
