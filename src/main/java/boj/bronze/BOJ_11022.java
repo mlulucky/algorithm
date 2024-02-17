6da07569d702e267746287c2853ec4d63b563dbd
mlulucky <dmswjdans@gmail.com>
2024-02-18 00:04:59 +0900

    fix: A+B-8

--- src/main/java/boj/bronze/BOJ_11022.java
+++ src/main/java/boj/bronze/BOJ_11022.java
@@ -10,10 +10,8 @@ public class BOJ_11022 {
     int T = sc.nextInt();
     sc.nextLine();
     for (int i = 1; i <= T; i++) {
-      String numStr = sc.nextLine();
-      String[] strArr = numStr.split(" ");
-      int A = Integer.parseInt(strArr[0]);
-      int B = Integer.parseInt(strArr[1]);
+      int A = sc.nextInt();
+      int B = sc.nextInt();
       int sum = A + B;
       sb.append(String.format("Case #%d: %d + %d = %d\n", i, A, B, sum));
     }
