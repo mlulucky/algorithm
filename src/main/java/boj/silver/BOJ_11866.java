5a391c04f6949dd3385dcc39be85d4d106abb984
mlulucky <dmswjdans@gmail.com>
2024-02-25 23:53:15 +0900

    요세푸스 문제

---
+++ src/main/java/boj/silver/BOJ_11866.java
@@ -0,0 +1,27 @@
+package boj.silver;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_11866 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    Queue<Integer> q = new LinkedList<>();
+    StringBuilder sb = new StringBuilder();
+    for(int i=1; i<=N; i++) {
+      q.offer(i);
+    }
+    sb.append("<");
+    while(q.size() > 1) {
+      for(int i=0; i<K-1; i++) {
+        q.offer(q.poll());
+      }
+      sb.append(q.poll() + ", ");
+    }
+    sb.append(q.poll() + ">");
+    System.out.println(sb);
+  }
+}
