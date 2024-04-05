625b0320cb35e24c9912a737781d96138fbbb611
mlulucky <dmswjdans@gmail.com>
2024-04-05 23:15:26 +0900

    꼬마 정민

---
+++ src/main/java/boj/bronze/BOJ_11382.java
@@ -0,0 +1,18 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11382 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    long res =0;
+    while(st.hasMoreTokens()) {
+      res+=Long.parseLong(st.nextToken());
+    }
+    System.out.println(res);
+  }
+}
