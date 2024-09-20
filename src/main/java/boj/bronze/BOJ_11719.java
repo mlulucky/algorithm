d39ad9d77dfaf090a02793594ce5a5bce7168b66
mlulucky <dmswjdans@gmail.com>
2024-09-20 12:53:29 +0900

    그대로 출력하기 2

---
+++ src/main/java/boj/bronze/BOJ_11719.java
@@ -0,0 +1,20 @@
+package boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s = br.readLine())!= null) {
+            System.out.println(s);
+        }
+    }
+}
+
+
+// break : 반복문 또는 switch 문 종료
+// continue : 현재 반복은 건너띄고, 다음 반복 진행
+// return : 메서드 종료. 값 반환
